6acc8796e410f3ef030339aaca90db50d9a17549
aicha652 <aichaazlf652@gmail.com>
2023-12-26 03:46:14 -0800

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,20 +2,21 @@
 """ script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def state_funct():
     """define state_funct function"""
-    states = storage.all(State)
+    states = models.storage.all(State)
     render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_funct():
+def teardown_funct(self):
     """Define a teardown_funct"""
-    storage.close()
+    self.storage.close()
 
 
 if __name__ == '__main__':

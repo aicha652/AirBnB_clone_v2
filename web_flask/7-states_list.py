24c1d02cfeedb1e386d11b9d9dda1c0929f10629
aicha652 <aichaazlf652@gmail.com>
2023-12-26 12:40:22 -0800

    fix task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def state_funct():
     """define state_funct function"""
-    states = storage.all(State).values()
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
